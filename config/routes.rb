c227d9a3955826a18276595f29993ab0a52259a5
haochen <haochen@containerdoor.co.nz>
2018-09-10 14:05:17 +0800

    routing error

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :admin do
-    resources :article
+    resources :articles
     resources :article_categories
   end
   # mount Fae below your admin namespec
